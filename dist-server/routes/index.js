fdbde30dfa83a4015e8dcba1abe9875b32afa879
timbrook168 <timbrook168@gmail.com>
2023-02-19 00:59:33 +0800

    change 修改发布脚本

--- dist-server/routes/index.js
+++ dist-server/routes/index.js
@@ -7,8 +7,6 @@ exports["default"] = void 0;
 var _express = _interopRequireDefault(require("express"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 var router = _express["default"].Router();
-
-/* GET home page. */
 router.get('/', function (req, res, next) {
   res.send("success");
 });
