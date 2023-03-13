4bb0f7c69ebc3ef8408abcf3488489de074244ea
timbrook168 <timbrook168@gmail.com>
2023-03-13 15:26:43 +0800

    change ChatGPTClient版本回滚

--- dist-server/app.js
+++ dist-server/app.js
@@ -8,7 +8,7 @@ var _express = _interopRequireDefault(require("express"));
 var _path = _interopRequireDefault(require("path"));
 var _cookieParser = _interopRequireDefault(require("cookie-parser"));
 var _morgan = _interopRequireDefault(require("morgan"));
-var _ChatGPTClient = _interopRequireDefault(require("./chatgpt/ChatGPTClient"));
+var _ChatGPTClientV = _interopRequireDefault(require("./chatgpt/ChatGPTClientV1"));
 var _index = _interopRequireDefault(require("./routes/index"));
 var _chat = _interopRequireDefault(require("./routes/gpt/chat"));
 var _config = _interopRequireDefault(require("./routes/gpt/config"));
@@ -25,6 +25,6 @@ app.use(_express["default"]["static"](_path["default"].join(__dirname, '../publi
 app.use('/', _index["default"]);
 app.use('/gpt/chat', _chat["default"]);
 app.use('/gpt/config', _config["default"]);
-global.gptClient = new _ChatGPTClient["default"](_config2.GPT_KEY);
+global.gptClient = new _ChatGPTClientV["default"](_config2.GPT_KEY);
 var _default = app;
 exports["default"] = _default;
